e77cde04cbe18c1407c9c98b1a7548840098304f
nyx <nnyyxxxx@protonmail.com>
2024-10-10 12:44:46 -0400

    improve load times

--- extra/keybindapp/run.sh
+++ extra/keybindapp/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-cd ~/hyprland/extra/keybindapp && python -m venv venv && . ~/hyprland/extra/keybindapp/venv/bin/activate && pip install PyQt6 && exec python keybinds.py
+if [ ! -d ~/hyprland/extra/keybindapp/venv ]; then
+    cd ~/hyprland/extra/keybindapp && python -m venv venv && . ~/hyprland/extra/keybindapp/venv/bin/activate && pip install PyQt6 && exec python keybinds.py
+else
+    cd ~/hyprland/extra/keybindapp && . ~/hyprland/extra/keybindapp/venv/bin/activate && exec python keybinds.py
+fi
