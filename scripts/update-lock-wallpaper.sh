ffd6c91ee007d0d6242022cbfb8c4306660fd34a
nyx <nnyyxxxx@protonmail.com>
2024-11-03 15:19:42 -0500

    fix script regex

--- scripts/update-lock-wallpaper.sh
+++ scripts/update-lock-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ln -sf "$(cat $HOME/hyprland/extra/hyprwall/config.ini | grep last_wallpaper | cut -d'=' -f2 | xargs)" "$HOME/hyprland/extra/hyprwall/last_wallpaper"
+ln -sf "$(eval echo $(cat $HOME/hyprland/extra/hyprwall/config.ini | grep last_wallpaper | cut -d'=' -f2 | xargs))" "$HOME/hyprland/extra/hyprwall/last_wallpaper"
 exec hyprlock
