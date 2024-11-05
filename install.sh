0d81995241111c80ad402733151f7ab5b6f59be1
nyx <nnyyxxxx@protonmail.com>
2024-11-05 00:34:49 -0500

    add wlogout

--- install.sh
+++ install.sh
@@ -96,7 +96,8 @@ installDeps() {
     current_step=$((current_step + 1))
 
     $AUR_HELPER -S --needed --noconfirm \
-        cava pipes.sh checkupdates-with-aur librewolf-bin hyprwall-bin python-pywalfox-librewolf > /dev/null 2>&1 || { printf "%b\n" "${RED}Failed to install AUR dependencies.${RC}"; }
+        cava pipes.sh checkupdates-with-aur librewolf-bin hyprwall-bin wlogout \
+        python-pywalfox-librewolf > /dev/null 2>&1 || { printf "%b\n" "${RED}Failed to install AUR dependencies.${RC}"; }
     printf "%b\n" "${GREEN}AUR dependencies installed (${current_step}/${total_steps})${RC}"
 }
 
