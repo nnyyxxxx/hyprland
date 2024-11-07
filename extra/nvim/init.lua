beaecdaccc3aff1a0a9b8025aa3853a7623418e2
nyx <nnyyxxxx@protonmail.com>
2024-11-06 20:18:31 -0500

    redo nvim configuration

--- extra/nvim/init.lua
+++ extra/nvim/init.lua
@@ -1,12 +1,10 @@
--- init.lua
-
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
@@ -22,62 +20,18 @@ require("lazy").setup({
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
   },
 
   { import = "plugins" },
-
-  {
-    "NvChad/base46",
-    branch = "v2.5",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local ok, base46 = pcall(require, "base46")
-      if ok then
-        if type(base46.load_theme) == "function" then
-          base46.load_theme()
-        else
-          print("base46.load_theme is not a function. base46 version: " .. (base46.VERSION or "unknown"))
-        end
-      else
-        print("Failed to load base46: " .. tostring(base46))
-      end
-    end,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-    config = function()
-      require('nvim-tree').setup {}
-    end,
-  },
 }, lazy_config)
 
 -- load theme
-local ok, _ = pcall(dofile, vim.g.base46_cache .. "defaults")
-if not ok then
-  print("Failed to load defaults from base46_cache")
-end
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
 
-ok, _ = pcall(dofile, vim.g.base46_cache .. "statusline")
-if not ok then
-  print("Failed to load statusline from base46_cache")
-end
-
+require "options"
 require "nvchad.autocmds"
 
 vim.schedule(function()
   require "mappings"
 end)
-
-vim.cmd [[
-  augroup NvimTree
-    autocmd!
-    autocmd VimEnter * NvimTreeToggle
-  augroup END
-]]
-
--- extra/nvim/lua/plugins/init.lua
+++ extra/nvim/lua/plugins/init.lua
@@ -2,44 +2,17 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    opts = require "configs.conform",
   },
 
+  -- These are some examples, uncomment them if you want to see them work!
   {
-    "nvim-lua/plenary.nvim",
-    module = "plenary",
-    setup = function()
-      vim.api.nvim_create_autocmd("Signal", {
-        pattern = "SIGUSR1",
-        callback = function()
-          require('nvchad.utils').reload()
-        end
-      })
-      
-      os.execute("python ~/.config/nvim/pywal/chadwal.py &> /dev/null &")
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
     end,
   },
 
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
