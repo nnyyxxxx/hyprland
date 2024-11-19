4e3cfe8a2f5a23eb66181b7fc1a7777cfbea6f9e
nnyyxxxx <nnyyxxxx@protonmail.com>
2024-11-19 08:22:08 -0500

    add new pfp

--- install.sh
+++ install.sh
@@ -224,7 +224,7 @@ setupConfigurations() {
 setupSDDMPfp() {
     printf "%b\n" "${YELLOW}:: Setting up SDDM profile picture...${RC}"
     $ESCALATION_TOOL mkdir -p /var/lib/AccountsService/icons/
-    $ESCALATION_TOOL cp "$HYPRLAND_DIR/pfps/elf.png" "/var/lib/AccountsService/icons/$USERNAME"
+    $ESCALATION_TOOL cp "$HYPRLAND_DIR/pfps/broke-mcdonalds-worker.jpg" "/var/lib/AccountsService/icons/$USERNAME"
 
     $ESCALATION_TOOL mkdir -p /var/lib/AccountsService/users/
     echo "[User]" | $ESCALATION_TOOL tee "/var/lib/AccountsService/users/$USERNAME" >/dev/null
