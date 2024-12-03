6e70de1c964b657cf61315eb4fbba9f4a7687805
nnyyxxxx <nnyyxxxx@protonmail.com>
2024-12-03 05:11:23 -0500

    fix conflict
    bonk

--- install.sh
+++ install.sh
@@ -158,7 +158,7 @@ install_deps() {
         libldap lib32-libldap openal lib32-openal libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva \
         ncurses lib32-ncurses vulkan-icd-loader lib32-vulkan-icd-loader ocl-icd lib32-ocl-icd libva lib32-libva \
         gst-plugins-base-libs lib32-gst-plugins-base-libs sdl2 lib32-sdl2 v4l-utils lib32-v4l-utils sqlite \
-        lib32-sqlite vulkan-radeon amdvlk lib32-mangohud mangohud pavucontrol qt6ct >/dev/null 2>&1 || { printf "%b\n" "${RED}:: Failed to install dependencies.${RC}"; }
+        lib32-sqlite vulkan-radeon lib32-vulkan-radeon lib32-mangohud mangohud pavucontrol qt6ct >/dev/null 2>&1 || { printf "%b\n" "${RED}:: Failed to install dependencies.${RC}"; }
     printf "%b\n" "${GREEN}:: Dependencies installed (${current_step}/${total_steps})${RC}"
     current_step=$((current_step + 1))
 
